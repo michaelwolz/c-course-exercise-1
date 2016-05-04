ac4abb8a6a3213254d1399cb836da23b79b6aea7
Michael Wolz <michaelwolz93@gmail.com>
2016-05-04 11:01:51 +0200

    Refractored x

--- aufgabe3.c
+++ aufgabe3.c
@@ -6,54 +6,54 @@
 ******************************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 
 char utf8_to_entities(char *utf8) {
-	char *x = utf8;
 	int state = 0;
 	unsigned int utf8character;
 
-	while (*x) {
+	while (*utf8) {
 		if (state > 0) {
 			if (state>1) 
-				utf8character = (utf8character + (*x & 0x3F))<<6;
+				utf8character = (utf8character + (*utf8 & 0x3F))<<6;
 			else
-				utf8character = utf8character + (*x & 0x3F);				
+				utf8character = utf8character + (*utf8 & 0x3F);				
 
 			printf("UTF-8 (Hex): %x\n", utf8character);
 			printf("UTF-8 (Dec): %d\n", utf8character);
 
-			if ((*x & 0xC0) != 0x80) 
+			if ((*utf8 & 0xC0) != 0x80) 
 				return 0;
 			
 			printf("\n");
 			state--;
-			x++;
+			utf8++;
 			continue;
 		}
 
-		if ((*x & 0x80) == 0) {
-			x++;
+		if ((*utf8 & 0x80) == 0) {
+			utf8++;
 			continue;
 		}
 
-		if ((*x & 0xE0) == 0xC0) {
+		if ((*utf8 & 0xE0) == 0xC0) {
 			state = 1;
-			utf8character = (*x & 0x1F)<<6;
-			x++;
+			utf8character = (*utf8 & 0x1F)<<6;
+			utf8++;
 			continue;
 		}
 
-		if ((*x & 0xF0) == 0xE0) {
+		if ((*utf8 & 0xF0) == 0xE0) {
 			state = 2;
-			utf8character = (*x & 0xF)<<6;
-			x++;
+			utf8character = (*utf8 & 0xF)<<6;
+			utf8++;
 			continue;
 		}
 
-		if ((*x & 0xF8) == 0xF0) {
+		if ((*utf8 & 0xF8) == 0xF0) {
 			state = 3;
-			utf8character = (*x & 0x7)<<6;
-			x++;
+			utf8character = (*utf8 & 0x7)<<6;
+			utf8++;
 			continue;
 		}
 		return 0;
